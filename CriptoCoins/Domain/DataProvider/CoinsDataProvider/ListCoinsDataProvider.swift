9d0e92d568a06dfca95a2011a5b43447ca7aa365
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-05-02 09:13:07 -0300

    Change: Finalizando ListCoinsDataProvider e GenericDataProvider!

--- CriptoCoins/Domain/DataProvider/CoinsDataProvider/ListCoinsDataProvider.swift
+++ CriptoCoins/Domain/DataProvider/CoinsDataProvider/ListCoinsDataProvider.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+protocol ListCoinsDataProviderDelegate: GenericDataProviderDelegate {}
+
+class ListCoinsDataProvider: DataProviderManager<GenericDataProviderDelegate, [CoinModel]> {
+    
+    private let coinsStore: CoinsStoreProtocol?
+    
+    init(coinsStore: CoinsStoreProtocol = CoinsStore()) {
+        self.coinsStore = coinsStore
+    }
+    
+    func fetchListCoins(by vsCurrency: String,
+                        with crytocurrency: [String]?,
+                        orderBy order: String,
+                        total parPage: Int,
+                        page: Int,
+                        percentagePrice: String) {
+        coinsStore?.fetchLisCoins(by: vsCurrency,
+                                  with: crytocurrency,
+                                  orderBy: order,
+                                  total: parPage,
+                                  page: page,
+                                  percentagePrice: percentagePrice,
+                                  completion: { result, error in
+            if let error = error {
+                self.delegete?.errorData(self.delegete, error: error)
+            }
+            
+            if let result = result {
+                self.delegete?.success(model: result)
+            }
+        })
+    }
+
+}
